8d231c88d68df39cdf432bc0e23653aa494cabdf
torogeldi <ttorogeldi@gmail.com>
2022-02-12 23:44:37 +0600

    base

---
+++ src/schema/resolvers.ts
@@ -0,0 +1,30 @@
+import {v4 as uuid} from "uuid";
+import Quote from "../models/Quote";
+// Provide resolver functions for your schema fields
+export const resolvers = {
+    Mutation: {
+        addQuote: async (parent: any, ctx: any) => {
+            const id = uuid();
+            const q = await Quote.create({id, phrase: ctx.phrase, quotee: ctx.quotee});
+            return {id: q.getDataValue("id"), phrase: q.getDataValue("phrase"), quotee: q.getDataValue("quotee")};
+        },
+        editQuote: async (parent: any, ctx: any) => {
+            const q = await Quote.update({phrase: ctx.phrase, quotee: ctx.quotee}, {
+                where: {id: ctx.id},
+                returning: true
+            });
+            return {
+                id: q[1][0].getDataValue("id"),
+                phrase: q[1][0].getDataValue("phrase"),
+                quotee: q[1][0].getDataValue("quotee")
+            };
+        },
+        deleteQuote: async (parent: any, ctx: any) => {
+            const q = await Quote.destroy({where: {id: ctx.id}});
+            return {ok: q};
+        }
+    },
+    Query: {
+        quotes: async () => await Quote.findAll({})
+    }
+};
